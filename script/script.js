e1ee187f88f2095f451bae68b78572c34b40de7e
Clayton0205 <claytonadonis5@gmail.com>
2022-06-15 15:04:12 +0200

    mintosec

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const calculate = document.getElementById('calculate');
+calculate.addEventListener('click',()=>{
+    let minutes = document.getElementById('minutes').value;
+    let total = (minutes * 60 )
+    document.getElementById('display1').innerHTML =
+    total + " Seconds" ;
+}
+);
